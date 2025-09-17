78524cc6f11110c4129b546bf998c622ddd51ca4
Javier Ruiz <fjavierrj6@gmail.com>
2025-09-17 12:37:49 +0200

    ejercicio3

--- UD1_Introduccion_JS/script.js
+++ UD1_Introduccion_JS/script.js
@@ -1,7 +1,4 @@
-function pintar_ruta() {
-    console.log("El acceso a la ruta C:\\usuario\\ tarda 1,23 segundos, algo considerado \"lento\" en la actualidad.");
-}
-
+//ejercicio 1
 function saludar() {
     var edad = window.prompt("¿Cuál es tu edad?");
     if (edad >= 18) {
@@ -10,3 +7,16 @@ function saludar() {
         console.log("Eres menor de edad, teniendo " + edad + " años");
     }
 }
+
+//ejercicio 2
+function pintar_ruta() {
+    console.log("El acceso a la ruta C:\\usuario\\ tarda 1,23 segundos, algo considerado \"lento\" en la actualidad.");
+}
+
+//ejercicio 3
+function pintar_ruta_v2() {
+    var substring1 = "El acceso a la ruta C:\\usuario\\ tarda 1,23 segundos, algo";
+    var substring2 = "considerado \"lento\" en la actualidad.";
+    console.log(substring1 + " " + substring2);
+}
+
