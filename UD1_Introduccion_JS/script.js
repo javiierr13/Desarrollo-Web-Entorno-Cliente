8628cce8062a9cc1d54a0e799c109d4001d39861
Javier Ruiz <fjavierrj6@gmail.com>
2025-09-17 12:11:12 +0200

    primer comit entorno Cliente

---
+++ UD1_Introduccion_JS/script.js
@@ -0,0 +1,12 @@
+function pintar_ruta() {
+    console.log("El acceso a la ruta C:\\usuario\\ tarda 1,23 segundos, algo considerado \"lento\" en la actualidad.");
+}
+
+function saludar() {
+    var edad = window.prompt("¿Cuál es tu edad?");
+    if (edad >= 18) {
+        console.log("Eres mayor de edad, teniendo " + edad + " años");
+    } else {
+        console.log("Eres menor de edad, teniendo " + edad + " años");
+    }
+}
